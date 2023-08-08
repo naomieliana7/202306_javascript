baeb5d7264042a9de0145ffc751bd7cf55486c61
naomieliana7 <131322122+naomieliana7@users.noreply.github.com>
2023-08-07 21:24:27 -0400

    Add files via upload

---
+++ Dojonary/script.js
@@ -0,0 +1,11 @@
+function logout() {
+    this.document.getElementById("login").innerHTML = "Logout"
+}
+
+function removeBtn() {
+    this.document.getElementById("definition").remove("definition");
+}
+
+function alertBtn() {
+    alert("Ninja was liked")
+}
